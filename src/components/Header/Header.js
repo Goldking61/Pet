9b0112be5a373662611dfb576d7edc411b811eab
Leonid <>
2022-08-31 15:08:48 +0700

    new components (Header, Main, Footer)

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import "./Header.css";
+
+function Header() {
+    return(
+        <mainarea>
+            <links>
+                <h2 to="/hello  ">
+                    Link 1
+                </h2>
+                <h2>
+                    Link 2
+                </h2>
+                <h2>
+                    Link 3
+                </h2>
+            </links>
+            <h1>Пет проект для портфолио</h1>
+        </mainarea>
+    );
+}
+
+export default Header;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,13 @@
+import "./Main.css";
+
+
+function Main() {
+    return(
+        <mainarea2>
+        <h1-main>Main</h1-main>
+        <h1-main>Page</h1-main>
+        </mainarea2>
+    );
+}
+
+export default Main;
