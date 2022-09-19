6607f70ae507dc55b8d24681e77274e120e2a419
Leonid <>
2022-09-19 14:36:03 +0700

    begin auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import {Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import {Switch, useHistory, Routes, Route, BrowserRouter as Router} from "react-router-dom";
+import { useState } from "react";
 import {Hello} from "./components/Hello/Hello.jsx";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -7,17 +8,27 @@ import { NotFound } from "./components/NotFound/NotFound";
 import { Css } from "./components/CssExamples/Css";
 import {MainPage} from "./components/MainPage/main.jsx";
 import Body from "./components/fon/fon.js";
+import { Auth } from "./components/Authorization/Auth.jsx";
 
 
 function App() {
+  const [auth, isAuth] = useState(false);
+  const history = useHistory();
+  const User = getCurrentUser();
+  console.log(auth)
   return (
-    
+    // Как правильно сделать?
     <Router>
       <Body>
       <Header/>
       <Routes>
+        <Route exact path="/" 
+          render={(props) => (
+          <Auth {...props} isAuth={isAuth}/>
+          )}
+          />
         <Route path="/hello" element={<Hello/>}/>
-        <Route path="/" element={<MainPage/>}/>
+        <Route path="/main" element={<MainPage/>}/>
         <Route path="/about" element={<About/>}/>
         <Route path="/css" element={<Css/>}/>
         <Route path="/*" element={<NotFound/>}/>
